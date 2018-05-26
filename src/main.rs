d61a0c7b12b344f2ce66d2eada41109144a8977d
mercury <udtrokia@163.com>
2018-05-27 01:46:06 +0800

    chapter 1 in one file with little annotation

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
 extern crate bytes;
-extern crate crypto;
 extern crate colored;
+extern crate crypto;
 
-use std::time::{SystemTime, UNIX_EPOCH};
+use colored::*;
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
-use colored::*;
-
-//use std::io::{self, Read};
-//use bytes::{Bytes};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 struct Block {
-    timestamp: String,
     data: String,
+    hash: String,
     prev_block_hash: String,
-    hash: String
+    timestamp: String
 }
 
 impl Block {
     fn set_hash(self) -> Block {
         let mut hasher = Sha256::new();
-        let header: String = String::new()  +
-            &self.timestamp + &self.data + &self.prev_block_hash;
+        let header: String = String::new()
+            + &self.timestamp + &self.data + &self.prev_block_hash;
+        // Mix header
         hasher.input_str(&header);
-
+        // Set hash
         return Block {
             hash: String::from(hasher.result_str().as_str()),
             ..self
@@ -31,14 +29,14 @@ impl Block {
     }
 }
 
-#[warn(unused_mut)]    
 fn new_block(data: String, prev_block_hash: String) -> Block {
     let block = Block {
-        timestamp: ts(),
         data: data,
-        prev_block_hash: prev_block_hash,
-        hash: String::new()
+        hash: String::new(),        
+        timestamp: ts(),
+        prev_block_hash: prev_block_hash
     };
+    // Cover the reserve hash.
     block.set_hash()
 }
 
@@ -51,47 +49,30 @@ struct Blockchain {
 }
 
 impl Blockchain {
-    #[warn(dead_code)]
     fn get_prev_hash(&self) -> String {
         let prev_block: &Block = &self.blocks[self.blocks.len() -1];
         let prev_hash: String = (&prev_block.hash).to_string();
+        // Get pre-hash from the last block.
         return prev_hash;
     }
     fn add_block(mut self, data: String) -> Blockchain {
-        // to copy self with value to new RAM address
-        // use reference to get the value of _self but not move the ownership.
-        // let prev_block: &Block = &self.blocks[&self.blocks.len() -1];
-        // let prev_hash: String = (&prev_block.hash).to_string();
-        // &self.blocks[&self.blocks.len() - 1]; // immutable
-
         let _prev_hash: String = self.get_prev_hash();
         let _new_block: Block = new_block(data, String::from(_prev_hash));
-        // let _new_block: Block = new_block(data, _prev_hash.to_string());
-        // println!("  new block data: {:?}", _new_block.data);
-        // let _blocks = (self.blocks).clone();
-
         self.blocks.push(_new_block);
+        // Push new block into blockchain.
         return Blockchain {
             blocks: self.blocks
         };
-        // self.blocks.push(_new_block)
-        // _blocks.push(_new_block)
-        // return Blockchain {}
-        // let _block: Block = new_genesis_block();
-        // println!("  new genesisblock hash:\n {:?}", _block.hash);
-        // This is a big problem!!! I need no mut but this.
-        // self.blocks.push(_new_block)
     }
 }
 
 fn new_blockchain() -> Blockchain {
     let mut _new_blockchain = Blockchain {
-        // blocks: vec![new_genesis_block()]
         blocks: vec![new_genesis_block()]
     };
+    // Generate a new blockchain.
     return _new_blockchain;
 }
-
 
 fn ts() -> String {
     let start = SystemTime::now();
@@ -99,29 +80,19 @@ fn ts() -> String {
         .expect("Time went backwards");
     let in_ms = since_the_epoch.as_secs() * 1000 +
         since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+    // Just fetch the UNIX time.
     return in_ms.to_string()
 }
 
-//#[derive(Debug)]
 fn main() {
-    // let blockchain: &Blockchain = &mut new_blockchain();
-    // let a:Blockchain = blockchain.add_block(String::from("hello, world"));
     let mut blockchain: Blockchain = new_blockchain();        
-    // {
-    //     let mut _add_block = |_data: String| blockchain.add_block(_data);
-    //     _add_block(String::from("hello, world"));
-    //     _add_block(String::from("hello, world"));        
-    // }
+    // Generate a new blockchain in main function.
     blockchain = blockchain.add_block(String::from("Hi, Pluto. I'm Basquiat."));
     blockchain = blockchain.add_block(String::from("Can you be my friend?"));
+    // Print block infos.
     for _block in blockchain.blocks {
         println!("\nPrev. hash: {}", _block.prev_block_hash.yellow());
 		    println!("Data: {}", _block.data.magenta());
 		    println!("Hash: {}\n", _block.hash.bold().underline().cyan());
     };
-    // blockchain.add_block(String::from("hello, world"));
-    // blockchain.add_block(String::from("hello, world"));
-    // let _block: Block = new_genesis_block();
-    // println!("{:?}", _block.hash)
-    // println!("{:?}", a.blocks[0].hash) 
 }
