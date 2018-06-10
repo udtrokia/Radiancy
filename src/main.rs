39b3681020f7d2a07b67c38553fd4af7b5e8c0d5
mercury <udtrokia@gmail.com>
2018-06-10 20:44:39 +0800

    empty main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@
   ** udtrokia<udtrokia@gmail.com>
   * 
  */
-extern crate colored; 
-extern crate radiancy; 
-
-use colored::*; 
-use radiancy::blockchain::blockchain::{Blockchain, new_blockchain};
-use radiancy::pow::pow::new_proof_of_work;
-use radiancy::num_bigint::{BigInt, Sign};
-
+//extern crate colored; 
+//extern crate radiancy; 
+//
+//use colored::*; 
+//use radiancy::blockchain::blockchain::{Blockchain, new_blockchain};
+//use radiancy::pow::pow::new_proof_of_work;
+//use radiancy::num_bigint::{BigInt, Sign};
+//
 fn main() {
-    let _blockchain: Blockchain = new_blockchain();
+//    let _blockchain: Blockchain = new_blockchain();
 //    blockchain = blockchain.add_block(String::from("This is Major Tom to Ground Control."));
 //    blockchain = blockchain.add_block(String::from("I am stepping through the door."));
 //    for _block in blockchain.blocks {
